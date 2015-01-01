c554b0869068d138294b473eb93b95ef34980893
noman777 <nomanahmed_1@yahoo.com>
2015-01-01 13:22:06 +0500

    chap4

---
+++ Chap4/CurrencyExchangeTest.java
@@ -0,0 +1,45 @@
+package com.sibi;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CurrencyExchangeTest {
+
+	@Test
+	public void testEquality() {
+		Dollar five= new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+	
+	@Test
+	public void testMultiplication() {
+		Dollar five= new Dollar(5);
+		Dollar product= five.times(2);
+		assertEquals(new Dollar(10), product);
+		product= five.times(3);
+		assertEquals(new Dollar(15), product);
+	}
+	
+	class Dollar {
+		
+		private int amount;
+		
+		Dollar(int amt) {
+			this.amount = amt;
+		}
+
+		Dollar times(int multiplier) {
+			return new Dollar(amount * multiplier);
+		}
+	
+		
+		public boolean equals(Object object) {			
+			Dollar dollar= (Dollar) object;
+			return amount == dollar.amount;
+		}
+
+	}
+}
