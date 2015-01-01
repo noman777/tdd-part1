b68896013bc252f5a937ebccfd1cb3942bbade6f
noman777 <nomanahmed_1@yahoo.com>
2015-01-01 15:48:18 +0500

    chap9

---
+++ Chap9/CurrencyExchangeTest.java
@@ -0,0 +1,40 @@
+package com.sibi;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CurrencyExchangeTest {
+
+	@Test
+	public void testEquality() {
+		
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		//assertTrue(new Franc(5).equals(new Franc(5)));
+		//assertFalse(new Franc(5).equals(new Franc(6)));
+		//assertFalse(new Franc(5).equals(Money.dollar(5)));
+	}	
+	
+	@Test
+	public void testMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10), five.times(2));
+		assertEquals(Money.dollar(15), five.times(3));
+	}
+	
+	@Test
+	public void testFrancMultiplication() {
+		Money five = Money.franc(5);
+		assertEquals(Money.franc(10), five.times(2));
+		assertEquals(Money.franc(15), five.times(3));
+		}
+	
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+	
+}
