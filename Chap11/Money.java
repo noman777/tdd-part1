05d6f8abd57907212ac07185b3e0f2f18cedef53
noman777 <nomanahmed_1@yahoo.com>
2015-01-02 12:23:47 +0500

    chap11

---
+++ Chap11/Money.java
@@ -0,0 +1,55 @@
+package com.sibi;
+
+class Money {
+
+	protected int amount;
+	protected String currency;
+
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
+	
+	static Money franc(int amount){
+		return new Money(amount, "CHF");
+	}
+
+	public String toString() {
+		return amount + " " + currency;
+	}
+
+
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount && currency().equals(money.currency());
+	}
+
+	protected String currency() {
+		return currency;
+	}
+
+}
+/*
+class Dollar extends Money {
+
+	Dollar(int amount, String currency) {
+		super(amount, currency);
+	}
+
+}
+
+class Franc extends Money {
+
+	Franc(int amount, String currency) {
+		super(amount, currency);
+	}
+
+}*/
