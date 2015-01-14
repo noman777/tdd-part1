42b376520d4f707f01fefbf50ac2e01d2a11bdf6
noman777 <nomanahmed_1@yahoo.com>
2015-01-14 15:41:47 +0500

    chap 4

--- CurrencyExchangeTest.java
+++ CurrencyExchangeTest.java
@@ -9,22 +9,24 @@ public class CurrencyExchangeTest {
 
 	@Test
 	public void testEquality() {
-		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		//assertFalse(new Dollar(5).equals(new Dollar(6)));
+		Dollar five= new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
 	
 	@Test
 	public void testMultiplication() {
-		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		assertEquals(10, product.amount);
-		product = five.times(3);
-		assertEquals(15, product.amount);
+		Dollar five= new Dollar(5);
+		Dollar product= five.times(2);
+		assertEquals(new Dollar(10), product);
+		product= five.times(3);
+		assertEquals(new Dollar(15), product);
 	}
 	
-	
-
 	class Dollar {
+		
+		private int amount;
+		
 		Dollar(int amt) {
 			this.amount = amt;
 		}
@@ -34,12 +36,10 @@ public class CurrencyExchangeTest {
 		}
 	
 		
-		public boolean equals(Object object) {
+		public boolean equals(Object object) {			
 			Dollar dollar= (Dollar) object;
 			return amount == dollar.amount;
 		}
-
-		private int amount;
 
 	}
 }
