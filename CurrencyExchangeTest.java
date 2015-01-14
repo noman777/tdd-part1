d71565c601079543cc0d772ac23403bffb81c54f
noman777 <nomanahmed_1@yahoo.com>
2015-01-14 15:39:38 +0500

    chap2 and chap3

--- CurrencyExchangeTest.java
+++ CurrencyExchangeTest.java
@@ -1,5 +1,6 @@
 package com.sibi;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -7,22 +8,38 @@ import org.junit.Test;
 public class CurrencyExchangeTest {
 
 	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		//assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+	
+	@Test
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
+	
+	
 
 	class Dollar {
 		Dollar(int amt) {
 			this.amount = amt;
 		}
 
-		void times(int multiplier) {
-		     this.amount = multiplier * amount;
+		Dollar times(int multiplier) {
+			return new Dollar(amount * multiplier);
+		}
+	
+		
+		public boolean equals(Object object) {
+			Dollar dollar= (Dollar) object;
+			return amount == dollar.amount;
 		}
 
-		int amount;
+		private int amount;
 
 	}
 }
