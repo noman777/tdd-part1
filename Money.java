9f4debe548c13e146d6013ade96907c6c7353cd0
noman777 <nomanahmed_1@yahoo.com>
2015-01-14 15:45:28 +0500

    chap 9

--- Money.java
+++ Money.java
@@ -1,42 +1,65 @@
 package com.sibi;
 
-
-
 abstract class Money {
 	abstract Money times(int multiplier);
-	protected int amount;
 
+	abstract String currency();
+
+	protected int amount;
+	protected String currency;
+	
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+		}
+	
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount,"USD");
 	}
 
 	static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
-	
+
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());
 	}
-	
+
 }
 
 class Dollar extends Money {
-	Dollar(int amt) {
-		this.amount = amt;
-	}
 	
+
+	
+	Dollar(int amount, String currency) {
+		super(amount, currency);
+	}
+
 	Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return Money.dollar(amount * multiplier);
+	}
+
+	@Override
+	String currency() {
+		return currency;
 	}
 
 }
 
 class Franc extends Money {
-	Franc(int amt) {
-		this.amount = amt;
+	
+
+	Franc(int amount, String currency) {
+		super(amount, currency);
 	}
+
 	Money times(int multiplier) {
-		return new Franc(amount * multiplier);
+		return Money.franc(amount * multiplier);
+	}
+
+	@Override
+	String currency() {
+		return currency;
 	}
 }
