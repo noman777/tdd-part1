ab0acfeed9a665e243013ab41eb5d89f5bfc2b64
noman777 <nomanahmed_1@yahoo.com>
2015-01-14 15:45:19 +0500

    chap 9

--- CurrencyExchangeTest.java
+++ CurrencyExchangeTest.java
@@ -12,9 +12,9 @@ public class CurrencyExchangeTest {
 		
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(new Franc(5).equals(new Franc(5)));
-		assertFalse(new Franc(5).equals(new Franc(6)));
-		assertFalse(new Franc(5).equals(Money.dollar(5)));
+		//assertTrue(new Franc(5).equals(new Franc(5)));
+		//assertFalse(new Franc(5).equals(new Franc(6)));
+		//assertFalse(new Franc(5).equals(Money.dollar(5)));
 	}	
 	
 	@Test
@@ -30,5 +30,11 @@ public class CurrencyExchangeTest {
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
 		}
+	
+	@Test
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
 	
 }
